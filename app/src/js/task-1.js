d110b80b0e2c36607769e19190306a2e1a25c40b
Vadym Chutur <falloutgreck@gmail.com>
2024-09-16 10:23:14 +0300

    upd

--- app/src/js/task-1.js
+++ app/src/js/task-1.js
@@ -8,4 +8,23 @@
 //   console.log(event.currentTarget.value);
 // }
 
-const formEl = document.querySelector;
+const formEl = document.querySelector('.js-form');
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const { userName, userEmail, userAge } = event.currentTarget.elements;
+  // console.log(event.currentTarget);
+  // console.dir(userName.value);
+  // console.dir(userEmail.value);
+  // console.dir(userAge.value);
+
+  const data = {
+    name: userName.value,
+    email: userEmail.value,
+    age: userAge.value,
+  };
+
+  console.log(data);
+}
